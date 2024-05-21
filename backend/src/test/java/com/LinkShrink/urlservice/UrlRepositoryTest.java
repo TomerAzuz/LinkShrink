3033d19d7590637931e06737ed5bf1d579a8b29a
TomerAzuz <tomerazuz1@gmail.com>
2024-05-21 17:15:30 +0300

    PostgreSQL testcontainers

--- backend/src/test/java/com/LinkShrink/urlservice/UrlRepositoryTest.java
+++ backend/src/test/java/com/LinkShrink/urlservice/UrlRepositoryTest.java
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Date;
 import java.util.Optional;
@@ -18,7 +23,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 public class UrlRepositoryTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("linkshrink_db")
+            .withUsername("user")
+            .withPassword("password");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgresDB::getDriverClassName);
+    }
 
     @Autowired
     private UrlRepository urlRepository;
